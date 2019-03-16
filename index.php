682ca18b6a92511e02d056c5b779ca92c786a701
muhrizky <muhrizqi@ce.undip.ac.id>
2019-03-16 18:47:31 +0700

    connection to database

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "<smart-parkir.database.windows.net>";
+    $user = "<dicoding>";
+    $pass = "<@Qwerty123>";
+    $db = "<Registration>";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);

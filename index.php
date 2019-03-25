41fd6860e7f24607ccbc7aeb388f3ddca2be302b
muhrizky <muhrizqi@ce.undip.ac.id>
2019-03-25 22:45:46 +0700

    change icon

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
     </nav>
 
     <main role="main" class="container">
-    <div class="starter-template"> 
+    <div class="starter-template"> <br><br><br>
         <h1>Smart Parkir Universitas Diponegoro</h1>
         <p class="lead">Isikan dengan lengkap dari <b>Nama NIM dan TNBK </b> anda.<br> Kemudian Click <b>Submit</b> untuk Registrasi Kendaraan anda.</p> <br>
         <span class="border-top my-3"></span>

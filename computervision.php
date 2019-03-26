8ac6c3769d538baf73ad000ced67fd820710b06d
muhrizky <muhrizqi@ce.undip.ac.id>
2019-03-26 15:37:54 +0700

    back againnn

--- computervision.php
+++ computervision.php
@@ -1,20 +1,29 @@
+<?php
+if (isset($_POST['submit'])) {
+	if (isset($_POST['url'])) {
+		$url = $_POST['url'];
+	} else {
+		header("Location: analyze.php");
+	}
+} else {
+	header("Location: analyze.php");
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Analyze Sample</title>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+	<title>Submission 2!</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 </head>
 <body>
- 
-<script type="text/javascript">
-    function processImage() {
+	<script type="text/javascript">
+		$(document).ready(function () {
         // **********************************************
         // *** Update or verify the following values. ***
         // **********************************************
- 
         // Replace <Subscription Key> with your valid subscription key.
-        var subscriptionKey = "6045712bcd97416ea3e76b7cb3cdce76";
- 
+        var subscriptionKey = "5244d0b5afb846009d2d0c8f14313367";
         // You must use the same Azure region in your REST API method as you used to
         // get your subscription keys. For example, if you got your subscription keys
         // from the West US region, replace "westcentralus" in the URL
@@ -25,72 +34,60 @@
         // this region.
         var uriBase =
         "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
-;
- 
         // Request parameters.
         var params = {
-            "visualFeatures": "Categories,Description,Color",
-            "details": "",
-            "language": "en",
+        	"visualFeatures": "Categories,Description,Color",
+        	"details": "",
+        	"language": "en",
         };
- 
         // Display the image.
-        var sourceImageUrl = document.getElementById("inputImage").value;
+        var sourceImageUrl = "<?php echo $url ?>";
         document.querySelector("#sourceImage").src = sourceImageUrl;
- 
         // Make the REST API call.
         $.ajax({
-            url: uriBase + "?" + $.param(params),
- 
+        	url: uriBase + "?" + $.param(params),
             // Request headers.
             beforeSend: function(xhrObj){
-                xhrObj.setRequestHeader("Content-Type","application/json");
-                xhrObj.setRequestHeader(
-                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+            	xhrObj.setRequestHeader("Content-Type","application/json");
+            	xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
             },
- 
             type: "POST",
- 
             // Request body.
             data: '{"url": ' + '"' + sourceImageUrl + '"}',
         })
- 
         .done(function(data) {
             // Show formatted JSON on webpage.
             $("#responseTextArea").val(JSON.stringify(data, null, 2));
+            // console.log(data);
+            // var json = $.parseJSON(data);
+            $("#description").text(data.description.captions[0].text);
         })
- 
         .fail(function(jqXHR, textStatus, errorThrown) {
             // Display error message.
             var errorString = (errorThrown === "") ? "Error. " :
-                errorThrown + " (" + jqXHR.status + "): ";
+            errorThrown + " (" + jqXHR.status + "): ";
             errorString += (jqXHR.responseText === "") ? "" :
-                jQuery.parseJSON(jqXHR.responseText).message;
+            jQuery.parseJSON(jqXHR.responseText).message;
             alert(errorString);
         });
-    };
+    });
 </script>
- 
+
 <h1>Analyze image:</h1>
-Enter the URL to an image, then click the <strong>Analyze image</strong> button.
-<br><br>
-Image to analyze:
-<input type="text" name="inputImage" id="inputImage"
-    value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
-<button onclick="processImage()">Analyze image</button>
-<br><br>
 <div id="wrapper" style="width:1020px; display:table;">
-    <div id="jsonOutput" style="width:600px; display:table-cell;">
-        Response:
-        <br><br>
-        <textarea id="responseTextArea" class="UIInput"
-                  style="width:580px; height:400px;"></textarea>
-    </div>
-    <div id="imageDiv" style="width:420px; display:table-cell;">
-        Source image:
-        <br><br>
-        <img id="sourceImage" width="400" />
-    </div>
+	<div id="jsonOutput" style="width:600px; display:table-cell;">
+		Response:
+		<br><br>
+		<textarea id="responseTextArea" class="UIInput"
+		style="width:580px; height:400px;" readonly=""></textarea>
+	</div>
+	<div id="imageDiv" style="width:420px; display:table-cell;">
+		Source image:
+		<br><br>
+		<img id="sourceImage" width="400" />
+		<br>
+		<h3 id="description">Loading description. . .</h3>
+	</div>
 </div>
 </body>
 </html>

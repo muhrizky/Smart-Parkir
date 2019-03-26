6c4a3a83aea3549d20eaa7a5bfc7cca6516a0818
muhrizky <muhrizqi@ce.undip.ac.id>
2019-03-26 22:41:31 +0700

    finishhhh

--- computervision.php
+++ computervision.php
@@ -108,7 +108,6 @@ if (isset($_POST['submit'])) {
         });
     </script>
 
-<h1>Analyze image:</h1>
 <div id="wrapper" style="width:1020px; display:table;">
 	<div id="jsonOutput" style="width:600px; display:table-cell;">
 		Response:
@@ -124,5 +123,9 @@ if (isset($_POST['submit'])) {
 		<h3 id="description">Loading description. . .</h3>
 	</div>
 </div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
+    <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
 </body>
 </html>

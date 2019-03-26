959bd856bae4d62b8f93571a70f0fcf8a90baee7
muhrizky <muhrizqi@ce.undip.ac.id>
2019-03-26 23:03:36 +0700

    oke backkkk

--- index.php
+++ index.php
@@ -18,12 +18,11 @@
   </head>
  <body>
  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
- <a class="navbar-brand" href="https://smart-parkir.azurewebsites.net/">Undip Smart Parkir</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="form-inline my-2 my-lg-0">
+        <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
             <a class="nav-link" href="https://smart-parkir.azurewebsites.net/">Home <span class="sr-only">(current)</span></a>
           </li>

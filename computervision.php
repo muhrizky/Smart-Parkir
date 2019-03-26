e108e8ed5e63b347de9e6fe10d24ae424e9ee003
muhrizky <muhrizqi@ce.undip.ac.id>
2019-03-26 12:42:30 +0700

    cahnge7

--- computervision.php
+++ computervision.php
@@ -63,21 +63,26 @@
     });
 </script>
 
-<h1>Analyze image:</h1>
-<div id="wrapper" style="width:1020px; display:table;">
-	<div id="jsonOutput" style="width:600px; display:table-cell;">
-		Response:
-		<br><br>
-		<textarea id="responseTextArea" class="UIInput"
-		style="width:580px; height:400px;" readonly=""></textarea>
-	</div>
-	<div id="imageDiv" style="width:420px; display:table-cell;">
-		Source image:
-		<br><br>
-		<img id="sourceImage" width="400" />
-		<br>
-		<h3 id="description">Loading description. . .</h3>
-	</div>
-</div>
+    <h1>Analyze image:</h1>
+        Enter the URL to an image, then click the <strong>Analyze image</strong> button.
+        <br><br>
+            Image to analyze:
+                <input type="text" name="inputImage" id="inputImage"
+                    value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
+                    <button onclick="processImage()">Analyze image</button>
+        <br><br>
+            <div id="wrapper" style="width:1020px; display:table;">
+                <div id="jsonOutput" style="width:600px; display:table-cell;">
+                Response:
+                <br><br>
+                <textarea id="responseTextArea" class="UIInput"
+                     style="width:580px; height:400px;"></textarea>
+        </div>
+            <div id="imageDiv" style="width:420px; display:table-cell;">
+                Source image:
+                <br><br>
+                <img id="sourceImage" width="400" />
+            </div>
+        </div>
 </body>
 </html>
